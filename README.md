42f17e72d66d119dba5183e03c21726925e469ef
Aryel Alves <mr.aryel@gmail.com>
2019-08-01 13:56:51 -0300

    added screenshots on readme and added date of docs on AtorVizu

--- README.md
+++ README.md
@@ -13,4 +13,13 @@ npm run dev
 
 # build for production with minification
 npm run build
+```
 
+## Screenshots
+
+<p align="center">
+  <img src="https://i.imgur.com/wrG81ma.png">
+  <img src="https://i.imgur.com/m3yjPkE.png">
+  <img src="https://i.imgur.com/uk8HHpc.png">
+  <img src="https://i.imgur.com/QAfgCoy.png">
+</p>
--- src/components/atores/AtorVisu.vue
+++ src/components/atores/AtorVisu.vue
@@ -50,6 +50,7 @@
 
                       <v-list-tile-content>
                         <v-list-tile-title>{{ item.arquivo }}</v-list-tile-title>
+                        <v-list-tile-sub-title>{{ item.data }}</v-list-tile-sub-title>
                       </v-list-tile-content>
 
                     </v-list-tile>
@@ -93,9 +94,6 @@ export default {
     }
   },
   methods: {
-    test () {
-      console.log('doc')
-    },
     cancel () {
       this.$store.state.visuAtor = false
     },
